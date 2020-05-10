9724550f9ac4a5c5214c27ad53257eb428b479e0
Mahmoud Fouad <mahmoudfouad443@gmail.com>
2020-05-10 02:21:41 +0200

    Initial commit

---
+++ Aqark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aqark
+//
+//  Created by Mahmoud Fouad on 5/10/20.
+//  Copyright © 2020 ITI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-AqarkTests/Pods-AqarkTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AqarkTests : NSObject
+@end
+@implementation PodsDummy_Pods_AqarkTests
+@end
