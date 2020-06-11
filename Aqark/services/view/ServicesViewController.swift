0af83af0c12b746ce235160fc3f42427fe8bcea6
ahmedsaeed <ahmed.saeed.rashed.1001@gmail.com>
2020-06-11 15:07:23 +0200

    services localization

--- Aqark/services/view/ServicesViewController.swift
+++ Aqark/services/view/ServicesViewController.swift
@@ -33,8 +33,11 @@ class ServicesViewController: UIViewController {
             servicesCollectionView.register(servicesNib, forCellWithReuseIdentifier: "Service Cell")
             servicesCollectionView.delegate = self
             servicesCollectionView.dataSource = self
+            print("serviceRole : " , serviceRole)
+            print("advertisemnt country : " , advertisementCountry)
+            
             servicesViewModel.getServiceUsers {
-                self.servicesViewModel.getServiceUsersList(serviceUserRole: self.serviceRole, country: self.advertisementCountry)
+                self.servicesViewModel.getServiceUsersList(serviceUserRole: self.serviceRole.localize, country: self.advertisementCountry.localize)
                 self.stopActivityIndicator()
                 UIView.animate(withDuration:2) {
                     self.view.alpha = 1
@@ -47,16 +50,16 @@ class ServicesViewController: UIViewController {
             }
         }else{
             noLabel.isHidden = false
-            noLabel.text = "Internet Connection Not Available."
+            noLabel.text = "Internet Connection Not Available".localize
         }
     }
     
     private func setLabelForZeroCount(){
         noLabel.isHidden = false
-        if(serviceRole == "Lawyers"){
-            noLabel.text = "No Lawyers Available."
+        if(serviceRole == "Lawyers".localize){
+            noLabel.text = "No Lawyers Available.".localize
         }else{
-            noLabel.text = "No Interior Designers Available."
+            noLabel.text = "No Interior Designers Available.".localize
         }
     }
 }
@@ -72,9 +75,27 @@ extension ServicesViewController : UICollectionViewDataSource,UICollectionViewDe
                   serviceCell.serviceUserDelegate = self
                   serviceCell.serviceUserName.text = servicesViewModel.serviceUsersViewList[indexPath.row].serviceUserName
                   serviceCell.serviceUserCompany.text = servicesViewModel.serviceUsersViewList[indexPath.row].serviceUserCompany
-                  serviceCell.serviceUserLocation.text = servicesViewModel.serviceUsersViewList[indexPath.row].serviceUserCountry
-                  serviceCell.serviceUserExperience.text =
-                      servicesViewModel.serviceUsersViewList[indexPath.row].serviceUserExperience + " years exp"
+        serviceCell.serviceUserLocation.text = servicesViewModel.serviceUsersViewList[indexPath.row].serviceUserCountry.localize
+        if "lang".localize.elementsEqual("en")
+        {
+            serviceCell.serviceUserExperience.text = self.convertNumbers(lang: "lang".localize, stringNumber: servicesViewModel.serviceUsersViewList[indexPath.row].serviceUserExperience).1 + " years exp".localize
+        }else{
+            let numberOfExp = self.convertNumbers(lang: "lang".localize, stringNumber: servicesViewModel.serviceUsersViewList[indexPath.row].serviceUserExperience).0.intValue
+            if numberOfExp == 0 {
+                serviceCell.serviceUserExperience.text = ""
+            }
+            else if numberOfExp == 1{
+
+                serviceCell.serviceUserExperience.text = "سنه خبره"
+            }else if numberOfExp == 2{
+
+                serviceCell.serviceUserExperience.text = "سنتين خبره"
+            }else{
+
+                serviceCell.serviceUserExperience.text = self.convertNumbers(lang: "lang".localize, stringNumber: String(numberOfExp)).1 + " years exp".localize
+            }
+        }
+        
                   serviceCell.serviceUserRating.rating =
                       servicesViewModel.serviceUsersViewList[indexPath.row].serviceUserServiceRating
                   let serviceUserImageURL = URL(string: servicesViewModel.serviceUsersViewList[indexPath.row].ServiceUserImage)
@@ -104,7 +125,7 @@ extension ServicesViewController:ServiceUsersCollectionDelegate{
         let serviceUserId = servicesViewModel.serviceUsersViewList[indexPath.row].serviceUserId
         let sameUser = servicesViewModel.checkServiceUser(serviceUserId: serviceUserId)
         if(sameUser){
-            showAlert(title:"User Error",message:"You can't rate yourself")
+            showAlert(title:"Error".localize ,message:"You can't rate yourself".localize)
             return true
         }
         return false
@@ -112,7 +133,7 @@ extension ServicesViewController:ServiceUsersCollectionDelegate{
     
     func showAlert(title:String,message:String){
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
-        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel){(okAction) in
+        let okAction = UIAlertAction(title: "Ok".localize, style: UIAlertAction.Style.cancel){(okAction) in
             alert.dismiss(animated: true, completion: nil)}
         alert.addAction(okAction)
         self.present(alert, animated: true, completion: nil)
