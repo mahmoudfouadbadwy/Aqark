34f848fddc809582e54367d406021a2196040d8f
Mahmoud Fouad <mahmoudfouad443@gmail.com>
2020-06-14 02:44:01 +0200

    configure login and first screen

--- Aqark/login/view /LoginViewController.swift
+++ Aqark/login/view /LoginViewController.swift
@@ -15,10 +15,9 @@ class LoginViewController: UIViewController{
     @IBOutlet weak var userPasswordTextField: CustomTextField!
     private var loginViewModel : LoginViewModel!
     var dataAccess : LoginDataAccessLayer!
-    var userRole : String!
-    
-   
-    
+    var profile : ProfileViewController!
+    var  firstScreen: FirstScreenViewController!
+    var adminView:AdminTabBarController!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
@@ -27,24 +26,33 @@ class LoginViewController: UIViewController{
     
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = false
-        dataAccess = LoginDataAccessLayer()
-        loginViewModel = LoginViewModel(dataAccess:dataAccess)
+        if ProfileNetworking.checkAuthuntication(){
+            if !ProfileNetworking.isAdmin()
+            {
+                self.profile = ProfileViewController()
+                self.navigationController?.pushViewController(profile, animated: true)
+            }
+        }else
+        {
+            dataAccess = LoginDataAccessLayer()
+            loginViewModel = LoginViewModel(dataAccess:dataAccess)
+        }
     }
     
     private func setupView() {
-           self.view.backgroundColor = UIColor(rgb: 0xf1faee)
-           dontHaveAccount.textColor = UIColor(rgb: 0x457b9d)
-           self.setTappedGesture()
-           self.navigationItem.title = "Login".localize
-       }
-       
-       private func setupTextFields() {
-           userEmailTextField.delegate = self
-           userPasswordTextField.delegate = self
-           self.userEmailTextField.setIcon(UIImage(named: "Email")!)
-           self.userPasswordTextField.setIcon(UIImage(named: "password")!)
-       }
-      
+        self.view.backgroundColor = UIColor(rgb: 0xf1faee)
+        dontHaveAccount.textColor = UIColor(rgb: 0x457b9d)
+        self.setTappedGesture()
+        self.navigationItem.title = "Login".localize
+    }
+    
+    private func setupTextFields() {
+        userEmailTextField.delegate = self
+        userPasswordTextField.delegate = self
+        self.userEmailTextField.setIcon(UIImage(named: "Email")!)
+        self.userPasswordTextField.setIcon(UIImage(named: "password")!)
+    }
+    
     @IBAction func login(_ sender: Any) {
         view.endEditing(true)
         self.showActivityIndicator()
@@ -74,10 +82,8 @@ class LoginViewController: UIViewController{
     }
     
     @IBAction func signUp(_ sender: Any) {
-        //Transfer user role to sign up view
-        let signUpView = SignUpView()
-        signUpView.role = userRole
-        self.navigationController?.pushViewController(signUpView, animated: true)
+        firstScreen = FirstScreenViewController()
+        self.navigationController?.pushViewController(firstScreen, animated: true)
     }
     
     func showAlert(title:String,message:String){
@@ -90,12 +96,12 @@ class LoginViewController: UIViewController{
     
     func gotoProfileView()
     {
-       let profileView:ProfileViewController = ProfileViewController()
-       self.navigationController?.pushViewController(profileView, animated: true)
+        profile = ProfileViewController()
+        self.navigationController?.pushViewController(profile, animated: true)
     }
-  
+    
     func gotoAdminView(){
-        let adminView:AdminTabBarController = AdminTabBarController()
+        adminView = AdminTabBarController()
         self.tabBarController?.tabBar.isHidden = true
         self.navigationController?.pushViewController(adminView, animated: true)
     }
@@ -118,12 +124,11 @@ class LoginViewController: UIViewController{
     override func viewWillDisappear(_ animated: Bool) {
         loginViewModel = nil
         dataAccess = nil
+        profile = nil
+        firstScreen =  nil
+        adminView = nil
     }
-    
-    deinit {
-        loginViewModel = nil
-        dataAccess = nil
-    }
+
 }
 
 extension LoginViewController : UITextFieldDelegate{
