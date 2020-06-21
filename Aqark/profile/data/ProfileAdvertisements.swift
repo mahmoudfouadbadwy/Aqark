9bfcfbc3fc438d37c0537115cc6d18dc07f11653
ahmedsaeed <ahmed.saeed.rashed.1001@gmail.com>
2020-06-21 22:59:30 +0200

    Update ProfileAdvertisements.swift

--- Aqark/profile/data/ProfileAdvertisements.swift
+++ Aqark/profile/data/ProfileAdvertisements.swift
@@ -49,7 +49,7 @@ extension ProfileDataAccess{
                     let size = value?["size"] as? String ?? ""
                     let images = value?["images"] as? [String] ?? ["NoImage"]
                     let payment = value?["payment"] as? String ?? ""
-                    let advertisementType = value?["AdvertisementType"] as? String ?? ""
+                    let advertisementType = value?["Advertisement Type"] as? String ?? ""
                     let advertisement:ProfileAdvertisement = ProfileAdvertisement(propertyType:propertyType,price:price,address:location,bed:bedrooms,bathroom:bathrooms,propertySize:size, propertyImage: images[0], paymentType: payment, advertisementType: advertisementType, advertisementId: id)
                     advertisementsStore.addAdvertisement(advertisement)
                     if advertisementsStore.allAdvertisements.count == arrOfIDs.count{
